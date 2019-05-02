ffb03a44ef5b6cb23aafaa8149468c30264ce0b0
Alexander Maul <alexander.maul@dwd.de>
2019-05-02 15:46:53 +0200

    Erweitere Log-Fenster+JS.

--- afd-log.js
+++ afd-log.js
@@ -1,26 +1,47 @@
 var AFDLOG = function() {
     return {
-        callAldaCmd : function(cmd) {
+        urlBase : "http://localhost:4080/",
+        callAldaCmd : function(ctx, cmd) {
             /*
              * Exec general AFD command, handle ajax call.
              */
-            console.log("callAfdCmd: ", cmd);
+            console.log("callAldaCmd: " + ctx + ": " + cmd);
             $.ajax({
                 type : "GET",
-                url : AFDCTRL.urlBase + "cmd",
+                url : AFDLOG.urlBase + "alda",
                 data : cmd,
-                success : function(a, b, c) {
-                    console.log(a + b + c);
-                }
+                success : function(data, status, jqxhr) {
+                    console.log(status);
+                    console.log(this);
+                    $(this).text(data);
+                    $(this).scrollTop($(this)[0].scrollHeight);
+                },
+                context : $("#" + ctx)
             });
         },
         callAldaLevel : function(log_name) {
             console.log("callAldaLevel " + log_name);
-            param=[];
+            var transl = {
+                info : "I",
+                config : "C",
+                warn : "W",
+                error : "E",
+                offline : "O",
+                debug : "D",
+            };
+            var filter = [];
             $.each($("#" + log_name + " ." + log_name + "_level"), function(i, v) {
-                v.value + "=" + v.checked;
+                if (v.checked) {
+                    filter.push(transl[v.value]);
+                }
             });
-            ;
+            if (filter.length == 0) {
+                alert("Check at least one log-level!");
+                return false;
+            }
+            var file_number = $("#" + log_name + "_logfile").get(0).value;
+            AFDLOG.callAldaCmd(log_name + "_area", "type=" + log_name + ";file=" + file_number + ";filter="
+                    + filter.join("|"));
         },
         callAldaFilter : function(log_name) {
             console.log("callAldaFilter " + log_name);

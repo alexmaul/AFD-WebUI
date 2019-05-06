00221cd2b5154f63b53af8124c9b0aff82610cac
Alexander Maul <alexander.maul@dwd.de>
2019-05-06 15:15:03 +0200

    Ausbau der Alias/AFD Kommando-Umsetzung/Verarbeitung.

--- ws/afd/ws/__init__.py
+++ ws/afd/ws/__init__.py
@@ -1,7 +1,8 @@
 import os.path
 from os import environ
 from shlex import split as shlex_split
-from flask import Flask, request, url_for, render_template, redirect, Markup, json, make_response
+from flask import (Flask, request, url_for, render_template,
+                   redirect, Markup, json, make_response)
 from subprocess import Popen, PIPE, CalledProcessError
 
 app = Flask(__name__)
@@ -18,14 +19,89 @@ def index():
 
 @app.route("/fsa/json", methods=["GET"])
 def fsa():
-    data = '{"data":' + exec_cmd("fsa_view_json", "plain") + "}"
-    app.logger.debug(data)
+    data = '{"data":' + exec_cmd("fsa_view_json", True) + "}"
     return make_response(data, {"Content-type": "application/json"})
 
 
-@app.route("/afd", methods=["POST"])
-def afd():
-    pass
+@app.route("/alias/<path:action>", methods=["POST", "GET"])
+def alias(action=None):
+    app.logger.debug("action: %s", action)
+    app.logger.debug(request.form)
+    cmd = "afdcmd"
+    cmd_opt = ""
+    if request.method == "POST":
+        alias_list = request.form["alias"].split(",")
+        if action == "start":
+            cmd_opt = "-t -q"
+        elif action == "stop":
+            cmd_opt = "-T -Q"
+        elif action == "able":
+            cmd_opt = "-X"
+        elif action == "debug":
+            cmd_opt = "-d"
+        elif action == "trace":
+            cmd_opt = "-c"
+        elif action == "fulltrace":
+            cmd_opt = "-C"
+        elif action == "switch":
+            cmd_opt = "-s"
+        elif action == "retry":
+            cmd_opt = "-r"
+        exec_cmd("{} {} {}".format(cmd, cmd_opt, " ".join(alias_list)), False)
+        return make_response("", 204, {"Content-type": "text/plain"})
+    elif request.method == "GET":
+        if action.startswith("info"):
+            cmd = "get_hostname "
+            cmd_opt = "-h"
+        elif action.startswith("config"):
+            cmd = "get_dc_data"
+            cmd_opt = "-h"
+        data = exec_cmd("{} {} {}".format(cmd, cmd_opt, action.split("/")[1]), True)
+        return make_response(data, {"Content-type": "text/plain"})
+    else:
+        return ""
+
+
+@app.route("/afd/<command>/<action>", methods=["GET", "POST"])
+def afd(command=None, action=None):
+    app.logger.debug("command: %s   action: %s", command, action)
+    app.logger.debug(request.form)
+    cmd = "afdcmd"
+    cmd_opt = ""
+    if request.method == "GET":
+        if command == "amg":
+            if action == "toggle":
+                cmd = "afdcmd"
+                cmd_opt = "-Y"
+        elif command == "fd":
+            if action == "toggle":
+                cmd = "afdcmd"
+                cmd_opt = "-Z"
+        elif command == "dc":
+            if action == "update":
+                cmd = "udc"
+                cmd_opt = ""
+        elif command == "hc":
+            if action == "update":
+                cmd = "uhc"
+                cmd_opt = ""
+        elif command == "afd":
+            if action == "start":
+                cmd = "afd"
+                cmd_opt = " -a"
+            elif action == "stop":
+                cmd = "afd"
+                cmd_opt = "-s"
+        exec_cmd("{} {}".format(cmd, cmd_opt))
+        return make_response("", 204, {"Content-type": "text/plain"})
+    elif request.method == "POST" and command == "hc" and action == "change":
+        r = save_hc(request.form)
+        if r:
+            make_response(r, 500, {"Content-type": "text/plain"})
+        else:
+            make_response("", 204, {"Content-type": "text/plain"})
+    else:
+        return None
 
 
 @app.route("/alda/<typ>", methods=["POST"])
@@ -38,37 +114,51 @@ def alda(typ=None):
         fnam = from_file[typ]
         fnum = request.form["file"]
         filt = request.form["filter"]
+        if (fnum == "all"):
+            fnum = "*"
         data = exec_cmd(
-            "grep -shP '<({})>' {}/log/{}{}".format(
+            "bash -c \"grep -shP '<({})>' {}/log/{}{}\"".format(
                 filt, AFD_WORK_DIR, fnam, fnum
                 ),
-            "plain"
+            True
             )
     else:
         par_tr = {
                "start":         "-t ",
                 "end":          "-T ",
                 "directory":    "-d ",
-                "recipient":    "-h %",
+                "recipient":    "-h ",
                 "filesize":     "-S ",
-                "jobid":        "",
-                "protocol":     "",
-                "only_received": "",
+                "jobid":        "-j ",
+                "protocol":     "-p ",
                 "only_archived": "",
-                "trans_time":   "",
+                "trans_time":   "-D ",
                 "delete_reason": "",
             }
         par_lst = []
+        fnam = ""
+        if request.form.get("only_received", None):
+            logtype = "R"
+        else:
+            logtype = typ[0].upper()
         app.logger.debug(request.form)
         for key, val in request.form.items():
-            par_lst.append(par_tr[key] + val)
-        cmd = "alda -f -L {} {}".format((typ[0]).upper(), " ".join(par_lst))
-        data = exec_cmd(cmd, "plain")
-    return data
+            if key == "filename":
+                fnam = val
+            elif key == "recipient":
+                rl = ",".join("%" + v for v in val.split(","))
+                par_lst.append("{}'{}'".format(par_tr[key], rl))
+            elif key in par_tr:
+                par_lst.append(par_tr[key] + val)
+        cmd = "alda -f -L {} {} {}".format(logtype,
+                                           " ".join(par_lst),
+                                           fnam)
+        data = exec_cmd(cmd, True)
+    return make_response(data, {"Content-type": "text/plain"})
 
 
-def exec_cmd(cmd, expect=None):
-    result = None
+def exec_cmd(cmd, read=False):
+    result = ""
     try:
         exec_args = shlex_split(cmd)
         app.logger.debug("prepare command: %s" % exec_args)
@@ -78,16 +168,21 @@ def exec_cmd(cmd, expect=None):
                    universal_newlines=True,
                    encoding="latin-1"
                    ) as xh_proc:
-            if expect is not None:
+            if read:
                 result = xh_proc.stdout.read()
             if xh_proc.wait() != 0:
                 app.logger.warning("command return: %d: %s",
                                xh_proc.wait(),
                                xh_proc.stderr.read()
                                )
+        app.logger.debug("cmd: %s -> len=%d", cmd, len(result))
     except CalledProcessError as e:
         app.logger.exception(e)
-    return result
+    return  result
+
+
+def save_hc(form):
+    pass
 
 
 def run():

2a4c983e801e383cbad33db4fd815feff624ef02
Alexander Maul <alexander.maul@dwd.de>
2019-05-29 15:05:33 +0200

    HC-edit-Seite fertigstellen, JS ausbauen.

--- ws/afd/webui/static/js/afd-edit.js
+++ ws/afd/webui/static/js/afd-edit.js
@@ -1,5 +1,14 @@
-var AFDEDITHC = function() {
+var AFDEDIT = function() {
     return {
+        /** urlBase. */
+        urlBase : "/",
+
+        /** urlView. */
+        urlHc: "afd/hc/",
+
+        /**
+         * 
+         */
         ableAll : function(obj, class_filter) {
             console.log("ableAll", obj, class_filter);
             if (obj.type == "radio") {
@@ -15,6 +24,38 @@ var AFDEDITHC = function() {
                     $("." + class_filter).attr("disabled", true);
                 }
             }
-        }
+        },
+
+        /**
+         * Exec general AFD command, handle ajax call.
+         */
+        readHostconfig : function(alias) {
+            console.log("readHostconfig: " + alias);
+            $.ajax({
+                type : "GET",
+                url : AFDEDIT.urlBase + AFDEDIT.urlHc + alias,
+                success : function(data, status, jqxhr) {
+                    console.log(status);
+                    $(this).html(data);
+                    $(this).find("tr").on("click", function(event) {
+                        $(this).toggleClass("selected");
+                    });
+                    $(this).scrollTop($(this)[0].scrollHeight);
+                },
+                error : function(status, jqxhr) {
+                    console.log(status, jqxhr);
+                },
+                complete : function(a, b) {
+                    $("#" + ctx + " .log_content-area-scroll .spinner-border").addClass("d-none");
+                },
+                context : $("#" + ctx + "-area")
+            });
+        },
+
     }; /* End returned object. */
 }();
+
+(function() {
+    $(document).ready(function() {
+    });
+})();

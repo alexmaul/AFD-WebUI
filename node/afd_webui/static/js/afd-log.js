50be938267f731f2d4d09d90f1cf0c838438dd60
amaul <alexander.maul@dwd.de>
2020-07-14 14:03:20 +0200

    Rewrite Log, Ajax->WS.

--- node/afd_webui/static/js/afd-log.js
+++ node/afd_webui/static/js/afd-log.js
@@ -1,228 +1,250 @@
 "use strict";
+/**
+ */
 var AFDLOG = function() {
-    return {
-        /** urlBase. */
-        urlBase : "/",
+	return {
+		/** urlBase. */
+		urlBase: "localhost:8040",
 
-        /** urlView. */
-        urlView : "view/",
+		/** urlView. */
+		urlView: "view/",
 
-        /** urlLog. */
-        urlLog : "log/",
+		/** selectedLogAreaLines. */
+		selectedLogAreaLines: {},
 
-        /** selectedLogAreaLines. */
-        selectedLogAreaLines : {},
-
-        /**
-         * Exec general AFD command, handle ajax call.
-         */
-        callAldaCmd : function(ctx, paramSet) {
-            console.log("callAldaCmd: " + ctx + ": " + paramSet);
-            $("#" + ctx + " *").css({
-                "cursor" : "progress"
-            });
-            $.ajax({
-                type : "POST",
-                url : AFDLOG.urlBase + AFDLOG.urlLog + ctx,
-                data : paramSet,
-                success : function(data, status, jqxhr) {
-                    console.log(ctx, status);
-                    $(this).html(data);
-                    $(this).find("tr").on("click", function(event) {
-                        $(this).toggleClass("selected");
-                    });
-                    $("." + ctx + "-area-scroll").scrollTop($(this)[0].scrollHeight);
-                },
-                error : function(status, jqxhr) {
-                    console.log(status, jqxhr);
-                },
-                complete : function(a, b) {
-                    $("#" + ctx + " *").css({
+		wsConnectionOpen: function() {
+			AFDLOG.ws = new WebSocket("ws://" + AFDLOG.urlBase + "/log", ["json"]),
+				AFDLOG.ws.addEventListener("open", function() {
+					console.log("ws-connection open");
+				});
+			AFDLOG.ws.addEventListener("close", function() {
+				alert("AFD closed connection!");
+			});
+			AFDLOG.ws.addEventListener("error", function(event) {
+				// error handler -> reconnect.
+				console.warn(event);
+			});
+			AFDLOG.ws.addEventListener("message", function(event) {
+				const message = JSON.parse(event.data);
+				console.log(message);
+				/* evaluate incoming message */
+				if (message.class == "log") {
+					let context = $("#" + message.context + "-area");
+					if (message.append) {
+						// TODO append or replace lines.
+					}
+					context.html(data);
+					context.find("tr").on("click", function(event) {
+						context.toggleClass("selected");
+					});
+					$("." + message.context + "-area-scroll").scrollTop($(this)[0].scrollHeight);
+					/* TODO progress-swirl?
+					$("#" + message.context + " *").css({
                         "cursor" : "auto"
                     });
-                },
-                context : $("#" + ctx + "-area")
-            });
-        },
+					*/
+				}
+			});
+		},
+
+		/**
+         * 
+         */
+		wsConnctionClose: function() {
+			console.debug("close ws connection.");
+		},
 
         /**
          * Retrieve full log-file content (e.g. system-log, transfer-log).
          */
-        callAldaLevel : function(logName) {
-            console.log("callAldaLevel " + logName);
-            var transl = {
-                info : "I",
-                config : "C",
-                warn : "W",
-                error : "E",
-                offline : "O",
-                debug : "D",
-            };
-            let filter = [];
-            $.each($("#" + logName + " ." + logName + "-level"), function(i, v) {
-                if (v.checked) {
-                    filter.push(transl[v.value]);
-                }
-            });
-            if (filter.length == 0) {
-                alert("Check at least one log-level!");
-                return false;
-            }
-            let fileNumber = $("#" + logName + "-logfile").get(0).value;
-            AFDLOG.callAldaCmd(logName, {
-                file : fileNumber,
-                filter : filter.join("|")
-            });
-        },
+		callAldaLevel: function(logName) {
+			console.log("callAldaLevel " + logName);
+			var transl = {
+				info: "I",
+				config: "C",
+				warn: "W",
+				error: "E",
+				offline: "O",
+				debug: "D"
+			};
+			let levelList = [];
+			$.each($("#" + logName + " ." + logName + "-level"), function(i, v) {
+				if (v.checked) {
+					levelList.push(transl[v.value]);
+				}
+			});
+			if (levelList.length == 0) {
+				alert("Select at least one log-level!");
+				return false;
+			}
+			let fileNumber = $("#" + logName + "-logfile").get(0).value;
+			AFDLOG.ws.send({
+				class: "log",
+				context: logName,
+				filter: {
+					file: fileNumber,
+					level: levelList.join("|")
+				}
+			});
+		},
 
         /**
          * Retrieve log information with ALDA.
          */
-        callAldaFilter : function(logName) {
-            console.log("callAldaFilter " + logName);
-            let paramSet = {};
-            $.each($("#" + logName + " .filter"), function(i, obj) {
-                if (obj.type == "checkbox") {
-                    if (obj.checked == true) {
-                        paramSet[obj.name] = obj.value;
-                    }
-                } else if (obj.value != "") {
-                    paramSet[obj.name] = obj.value;
-                }
-            });
-            console.log(paramSet);
-            AFDLOG.callAldaCmd(logName, paramSet);
-        },
+		callAldaFilter: function(logName) {
+			console.log("callAldaFilter " + logName);
+			let paramSet = {};
+			$.each($("#" + logName + " .filter"), function(i, obj) {
+				if (obj.type == "checkbox") {
+					if (obj.checked == true) {
+						paramSet[obj.name] = obj.value;
+					}
+				} else if (obj.value != "") {
+					paramSet[obj.name] = obj.value;
+				}
+			});
+			console.log(paramSet);
+			AFDLOG.ws.send({
+				class: "log",
+				context: logName,
+				filter: paramSet
+			});
+		},
 
-        /**
-         * Set input fields for start- and end-time according to range.
-         */
-        setDate : function(logName, timeRange) {
-            console.log("setDate", logName, timeRange);
-            if (timeRange == "clear") {
-                $("#" + logName + " .filter[name=start]").val("");
-                $("#" + logName + " .filter[name=end]").val("");
-                return;
-            }
-            let dateStart = "";
-            let dateEnd = "";
-            let now = new Date(Date.now());
-            let datetimeNowMonth = now.getMonth() + 1;
-            let datetimeNowDay = now.getDate();
-            let datetimeNowHour = now.getHours();
-            let datetimeNowMinute = now.getMinutes();
-            let yday = new Date(Date.now());
-            yday.setDate(now.getDate() - 1);
-            let datetimeYdayMonth = yday.getMonth() + 1;
-            let datetimeYdayDay = yday.getDate();
-            if (datetimeNowMonth < 10) {
-                datetimeNowMonth = '0' + datetimeNowMonth;
-            }
-            if (datetimeNowDay < 10) {
-                datetimeNowDay = '0' + datetimeNowDay;
-            }
-            if (datetimeNowHour < 10) {
-                datetimeNowHour = '0' + datetimeNowHour;
-            }
-            if (datetimeNowMinute < 10) {
-                datetimeNowMinute = '0' + datetimeNowMinute;
-            }
-            if (datetimeYdayMonth < 10) {
-                datetimeYdayMonth = '0' + datetimeYdayMonth;
-            }
-            if (datetimeYdayDay < 10) {
-                datetimeYdayDay = '0' + datetimeYdayDay;
-            }
-            if (timeRange == "today") {
-                dateStart = "" + datetimeNowMonth + datetimeNowDay + "0000";
-                dateEnd = "" + datetimeNowMonth + datetimeNowDay + datetimeNowHour + datetimeNowMinute;
-            } else if (timeRange == "yesterday") {
-                dateStart = "" + datetimeYdayMonth + datetimeYdayDay + "0000";
-                dateEnd = "" + datetimeNowMonth + datetimeNowDay + "0000";
-            }
-            $("#" + logName + " .filter[name=start]").val(dateStart);
-            $("#" + logName + " .filter[name=end]").val(dateEnd);
-            console.log(dateStart, dateEnd);
-        },
+		/**
+		 * Set input fields for start- and end-time according to range.
+		 */
+		setDate: function(logName, timeRange) {
+			console.log("setDate", logName, timeRange);
+			if (timeRange == "clear") {
+				$("#" + logName + " .filter[name=start]").val("");
+				$("#" + logName + " .filter[name=end]").val("");
+				return;
+			}
+			let dateStart = "";
+			let dateEnd = "";
+			let now = new Date(Date.now());
+			let datetimeNowMonth = now.getMonth() + 1;
+			let datetimeNowDay = now.getDate();
+			let datetimeNowHour = now.getHours();
+			let datetimeNowMinute = now.getMinutes();
+			let yday = new Date(Date.now());
+			yday.setDate(now.getDate() - 1);
+			let datetimeYdayMonth = yday.getMonth() + 1;
+			let datetimeYdayDay = yday.getDate();
+			if (datetimeNowMonth < 10) {
+				datetimeNowMonth = '0' + datetimeNowMonth;
+			}
+			if (datetimeNowDay < 10) {
+				datetimeNowDay = '0' + datetimeNowDay;
+			}
+			if (datetimeNowHour < 10) {
+				datetimeNowHour = '0' + datetimeNowHour;
+			}
+			if (datetimeNowMinute < 10) {
+				datetimeNowMinute = '0' + datetimeNowMinute;
+			}
+			if (datetimeYdayMonth < 10) {
+				datetimeYdayMonth = '0' + datetimeYdayMonth;
+			}
+			if (datetimeYdayDay < 10) {
+				datetimeYdayDay = '0' + datetimeYdayDay;
+			}
+			if (timeRange == "today") {
+				dateStart = "" + datetimeNowMonth + datetimeNowDay + "0000";
+				dateEnd = "" + datetimeNowMonth + datetimeNowDay + datetimeNowHour + datetimeNowMinute;
+			} else if (timeRange == "yesterday") {
+				dateStart = "" + datetimeYdayMonth + datetimeYdayDay + "0000";
+				dateEnd = "" + datetimeNowMonth + datetimeNowDay + "0000";
+			}
+			$("#" + logName + " .filter[name=start]").val(dateStart);
+			$("#" + logName + " .filter[name=end]").val(dateEnd);
+			console.log(dateStart, dateEnd);
+		},
 
-        /**
-         * Show or hide modal with selection form.
-         */
-        toggleModal : function(modal) {
-            $.each($("#" + modal + " input.form-check-input"), function(i, obj) {
-                if (obj.checked == true) {
-                    obj.checked = false;
-                } else {
-                    obj.checked = true;
-                }
-            });
-        },
+		/**
+		 * Show or hide modal with selection form.
+		 */
+		toggleModal: function(modal) {
+			$.each($("#" + modal + " input.form-check-input"), function(i, obj) {
+				if (obj.checked == true) {
+					obj.checked = false;
+				} else {
+					obj.checked = true;
+				}
+			});
+		},
 
-        /**
-         * Update selection form in modal on any change, for event-handler.
-         */
-        updateModal : function(modalId) {
-            let checkedList = [];
-            $.each($("#" + modalId + " .form-check-input"), function(i, obj) {
-                if (obj.checked) {
-                    checkedList.push(obj.value);
-                }
-            });
-            $("#" + modalId + "Value").attr("value", checkedList.join(","));
-        },
+		/**
+		 * Update selection form in modal on any change, for event-handler.
+		 */
+		updateModal: function(modalId) {
+			let checkedList = [];
+			$.each($("#" + modalId + " .form-check-input"), function(i, obj) {
+				if (obj.checked) {
+					checkedList.push(obj.value);
+				}
+			});
+			$("#" + modalId + "Value").attr("value", checkedList.join(","));
+		},
 
-        /**
-         * View file content for each selected one in a new window.
-         */
-        callView : function(logName) {
-            console.log("callView " + logName);
-            let selectedLogAreaLines = [];
-            $.each($("#" + logName + " .selected"), function(i, obj) {
-                if (obj.childNodes[obj.childElementCount - 1].innerText == "Y") {
-                    selectedLogAreaLines.push(obj.attributes["archive"].value);
-                }
-            });
-            if (selectedLogAreaLines.length == 0) {
-                alert("Select archived log entry first!");
-                return;
-            }
-            let mode = $("#" + logName + "-view-mode").text().split(" ")[1].toLowerCase();
-            console.log("view", mode, selectedLogAreaLines);
-            $.each(selectedLogAreaLines, function(i, v) {
-                window.open(AFDLOG.urlBase + AFDLOG.urlView + mode + "/" + v);
-            });
-        }
-    }; /* End returned object. */
+		/**
+		 * View file content for each selected one in a new window.
+		 */
+		callView: function(logName) {
+			console.log("callView " + logName);
+			let selectedLogAreaLines = [];
+			$.each($("#" + logName + " .selected"), function(i, obj) {
+				if (obj.childNodes[obj.childElementCount - 1].innerText == "Y") {
+					selectedLogAreaLines.push(obj.attributes["archive"].value);
+				}
+			});
+			if (selectedLogAreaLines.length == 0) {
+				alert("Select archived log entry first!");
+				return;
+			}
+			let mode = $("#" + logName + "-view-mode").text().split(" ")[1].toLowerCase();
+			console.log("view", mode, selectedLogAreaLines);
+			$.each(selectedLogAreaLines, function(i, v) {
+				window.open(AFDLOG.urlBase + AFDLOG.urlView + mode + "/" + v);
+			});
+		}
+	}; /* End returned object. */
 }();
 
 (function() {
-    $(document).ready(
-            function() {
-                /*
-                 * Activate tab if url-anchor is set.
-                 */
-                console.log(window.location);
-                if (window.location.hash != "") {
-                    console.log("found anchor: " + window.location.hash);
-                    $(window.location.hash + "-tab").tab("show");
-                }
-                /*
-                 * Pre-set input field "Recipient" if "?..." is set in URL.
-                 */
-                if (window.location.search != "") {
-                    console.log("found query: " + window.location.search);
-                    $("#" + window.location.hash.substring(1) + " .filter[name=recipient]").val(
-                            window.location.search.substring(1));
-                }
-                /*
-                 * Set update function for modal events.
-                 */
-                let modalList = [ "modalProtocol", "modalDelete" ];
-                for (let i = 0; i < modalList.length; i++) {
-                    $("#" + modalList[i]).on("hide.bs.modal", function(event) {
-                        AFDLOG.updateModal(event.target.id);
-                    });
-                    AFDLOG.updateModal(modalList[i]);
-                }
-            });
+	$(document).ready(
+		function() {
+			/*
+			 * Activate tab if url-anchor is set.
+			 */
+			console.log(window.location);
+			if (window.location.hash != "") {
+				console.log("found anchor: " + window.location.hash);
+				$(window.location.hash + "-tab").tab("show");
+			}
+			/*
+			 * Pre-set input field "Recipient" if "?..." is set in URL.
+			 */
+			if (window.location.search != "") {
+				console.log("found query: " + window.location.search);
+				$("#" + window.location.hash.substring(1) + " .filter[name=recipient]").val(
+					window.location.search.substring(1));
+			}
+			/*
+			 * Set update function for modal events.
+			 */
+			let modalList = ["modalProtocol", "modalDelete"];
+			for (let i = 0; i < modalList.length; i++) {
+				$("#" + modalList[i]).on("hide.bs.modal", function(event) {
+					AFDLOG.updateModal(event.target.id);
+				});
+				AFDLOG.updateModal(modalList[i]);
+			}
+			AFDLOG.urlBase = window.location.host;
+			AFDLOG.wsConnectionOpen();
+			$(document).on("close", function(event) {
+				AFDLOG.wsConnctionClose();
+			});
+		});
 })();

ee62bda0154f925219ab924dc876c011cbb9df31
amaul <amaul@OOFNBBD079.win2003.dwd.de>
2020-07-24 13:58:20 +0200

    Rewrite: alle Client-JS-Objekte in einer Datei zusammenfassen.

--- node/afd_webui/static/js/afd-gui.js
+++ node/afd_webui/static/js/afd-gui.js
@@ -767,40 +767,486 @@ var AFDCTRL = function() {
 	}; /* End returned object. */
 }();
 
-(function() {
-	$(document).ready(function() {
-        /*
-         * Set height for host area.
+var AFDEDIT = function() {
+	return {
+		/** Websocket protocol scheme. */
+		urlProto: "ws:",
+
+		/** urlBase. */
+		urlBase: "localhost:8040",
+
+		/** Hold Websocket connection. */
+		ws: {},
+
+        /**
+         * Enable/disable form-input-tags which have CSS-class as filter.
          */
-		let tabAreaHeight = $(window).innerHeight() - $("#navbarArea").innerHeight() - 40;
-		$("#tab-area").attr("style", "height:" + tabAreaHeight + "px;");
-        /*
-         * Set event-handler for navbar menu.
+		ableAll: function(obj, class_filter) {
+			console.debug("ableAll", class_filter);
+			if (obj.type == "radio") {
+				if (obj.value == "yes" && obj.checked == true) {
+					$("." + class_filter).removeAttr("disabled");
+				} else {
+					$("." + class_filter).attr("disabled", true);
+				}
+			} else {
+				if (obj.checked == true) {
+					$("." + class_filter).removeAttr("disabled");
+				} else {
+					$("." + class_filter).attr("disabled", true);
+				}
+			}
+		},
+
+		dupcheck_defaults: function() {
+			console.debug("dupcheck_defaults");
+			$("#dupcheck_timeout")[0].value = "3600";
+			$("#dupcheck_reference-alias")[0].checked = true;
+			$("#dupcheck_type-name")[0].checked = true;
+			$("#dupcheck_delete")[0].checked = true;
+			$("#dupcheck_crc-crc32")[0].checked = true;
+		},
+
+        /**
+         * Send host configuration data for the selected host for saving.
          */
-		$("nav").find("a").not(".dropdown-toggle").click(function(event) {
-			AFDCTRL.evalMenu(event.target.text);
-		});
-        /*
-         * Set update function for modal events.
+		updateHostconfig: function() {
+			console.info("updateHostconfig: selected alias:", $("#alias-list"));
+			let alias = $("#alias-list")[0].value;
+			if (alias == null || alias == "") {
+				console.debug("nothing to do.");
+				return;
+			}
+			let ol = $("#alias-list option");
+			let aliasList = [];
+			for (let i = 0; i < ol.length; i++) {
+				aliasList.push(ol[i].value);
+			}
+			let formData = {
+				order: aliasList,
+				data: {}
+			};
+			formData["data"][alias] = {};
+			$.each($(".filter"), function(i, obj) {
+				if (obj.type == "radio") {
+					if (obj.checked) {
+						formData["data"][alias][obj.id.split("-", 1)[0]] = obj.value;
+					}
+				} else if (obj.type == "checkbox") {
+					if (obj.checked) {
+						formData["data"][alias][obj.id] = "yes";
+					}
+				} else {
+					formData["data"][alias][obj.id] = obj.value;
+				}
+			});
+			formData["data"][alias]["alias"] = alias;
+			delete formData["data"][alias]["alias-list"];
+			const message = {
+				class: "afd",
+				action: "hc",
+				command: "save",
+				alias: alias !== null ? [alias] : [],
+				data: formData
+			};
+			console.debug(message);
+			AFDEDIT.ws.send(JSON.stringify(message));
+		},
+
+        /**
+         * Exec general AFD command, handle ajax call.
          */
-		$("#modalInfo").on("hide.bs.modal", function(event) {
-			console.debug("clear modalInfoBody");
-			$("#modalInfoBody").children().remove();
-		});
-        /*
-         * Initial load data.
+		readHostconfig: function(alias) {
+			console.debug("readHostconfig: " + alias);
+			const message = {
+				class: "afd",
+				action: "hc",
+				command: "read",
+				alias: alias !== null ? [alias] : []
+			};
+			AFDEDIT.ws.send(JSON.stringify(message));
+		},
+
+        /**
+         * Re-build the hostname list to reflect any change in the order.
          */
-		// AFDCTRL.loadData();
-        /*
-         * Set interval-handler to regularly load data and update display.
+		changeHostconfigAliasList: function(aliasList, selectedAlias) {
+			console.debug("changeHostconfigAliasList: " + aliasList + " - " + selectedAlias);
+			let data = "";
+			for (let i = 0; i < aliasList.length; i++) {
+				if (aliasList[i] == selectedAlias) {
+					data = data + "<option selected>" + aliasList[i] + "</option>";
+				} else {
+					data = data + "<option>" + aliasList[i] + "</option>";
+				}
+			}
+			$("#alias-list").html(data);
+		},
+
+        /**
+         * Update the form-input-tag values with retrieved values.
          */
-		if (window.location.protocol === "https:") {
-			AFDCTRL.urlProto = "wss:";
+		changeHostconfigFormValues: function(data, selectedAlias) {
+			console.debug("changeHostconfigFormValues: " + selectedAlias);
+			if (data[selectedAlias]["keep_connected_direction"] != "send"
+				&& data[selectedAlias]["keep_connected_direction"] != "fetch") {
+				data[selectedAlias]["keep_connected_direction"] = "both";
+			}
+			if (data[selectedAlias]["dupcheck_timeout"] == 0) {
+				data[selectedAlias]["dupcheck_flag"] = "disable";
+			} else {
+				data[selectedAlias]["dupcheck_flag"] = "enable";
+			}
+			$.each(data[selectedAlias], function(k, v) {
+				let objList = $("[name=" + k + "]");
+				if (objList.length > 0) {
+					if (objList[0].type == "radio") {
+						objList = $("#" + k + "-" + v);
+						if (objList.length > 0) {
+							objList[0].checked = true;
+						}
+					} else if (objList[0].type == "checkbox") {
+						objList = $("#" + k);
+						if (objList.length > 0 && v == "yes") {
+							objList[0].checked = true;
+						} else {
+							objList[0].checked = false;
+						}
+					} else {
+						objList[0].value = v;
+					}
+				}
+			});
+			AFDEDIT.ableAll($("#host_switch_enable")[0], "host_switch");
+			AFDEDIT.ableAll($("#dupcheck_flag-enable")[0], "dupcheck");
+		},
+
+        /**
+         * Move a hostname up/down in the hostname-list.
+         */
+		moveHostconfigAlias: function(direction) {
+			console.debug("moveHostconfigAlias", direction);
+			let alias = $("#alias-list")[0].value;
+			if (!alias) {
+				return;
+			}
+			let aliasList = $("#alias-list option");
+			console.debug("aliasList -------");
+			$.each(aliasList, function(i, v) {
+				console.debug(v.value);
+			});
+			let is = 0;
+			let ie = aliasList.length;
+			if (direction == -1) {
+				is = 1;
+			} else if (direction == 1) {
+				ie = ie - 1;
+			}
+			for (let i = is; i < ie; i++) {
+				console.debug("test", i, aliasList[i].value)
+				if (alias == aliasList[i].value) {
+					if (direction) {
+						console.debug("move", i, aliasList[i].value);
+						let buf = aliasList[i];
+						aliasList[i] = aliasList[i + direction];
+						aliasList[i + direction] = buf;
+					} else {
+						aliasList.splice(i, 1);
+					}
+					$("#alias-list").html(aliasList);
+					break;
+				}
+			}
+			console.debug("aliasList -------");
+			$.each(aliasList, function(i, v) {
+				console.debug(v.value);
+			});
+		},
+
+		wsConnectionOpen: function() {
+			AFDEDIT.ws = new WebSocket(
+				AFDEDIT.urlProto + "//" + AFDEDIT.urlBase + "/ctrl",
+				["json"]
+			);
+			AFDEDIT.ws.addEventListener("open", function() {
+				console.info("ws-connection open");
+				if (window.location.pathname.endsWith("afd-hcedit.html")) {
+					/*
+					 * Document-ready actions for Host-Config-Editor.
+					 */
+					if (window.location.search != "") {
+						/*
+						 * In case there"s "?<alias>" in URL, load this alias ...
+						 */
+						let al = window.location.search.substring(1).split(",");
+						AFDEDIT.readHostconfig(al[0]);
+					} else {
+						/*
+						 * ... otherwise load the first one.
+						 */
+						AFDEDIT.readHostconfig(null);
+					}
+				} else {
+					alert("Why is there no DIR_CONFIG editor???");
+				}
+			});
+			AFDEDIT.ws.addEventListener("close", function() {
+				alert("AFD closed connection!");
+			});
+			AFDEDIT.ws.addEventListener("error", function(event) {
+				// error handler -> reconnect.
+				console.warn(event);
+			});
+			AFDEDIT.ws.addEventListener("message", function(event) {
+				const message = JSON.parse(event.data);
+				console.debug(message);
+				/* evaluate incoming message */
+				if (message.class == "afd" && message.action == "hc") {
+					AFDEDIT.changeHostconfigAliasList(message.order, message.alias);
+					if (message.alias != null && message.alias != "") {
+						AFDEDIT.changeHostconfigFormValues(message.data, message.alias);
+					} else {
+						AFDEDIT.changeHostconfigFormValues(message.data, message.order[0]);
+					}
+				}
+			});
+		},
+
+		/**
+         * 
+         */
+		wsConnctionClose: function() {
+			console.debug("close ws connection.");
 		}
-		AFDCTRL.urlBase = window.location.host;
-		AFDCTRL.wsConnectionOpen();
-		$(document).on("close", function(event) {
-			AFDCTRL.wsConnctionClose();
-		});
-	});
-})();
+
+	}; /* End returned object. */
+}();
+
+var AFDLOG = function() {
+	return {
+		/** Websocket protocol scheme. */
+		urlWsProto: "ws:",
+
+		/** Protocol scheme for view-content. */
+		urlViewProto: "http:",
+
+		/** urlBase. */
+		urlBase: "localhost:8040",
+
+		/** selectedLogAreaLines. */
+		selectedLogAreaLines: {},
+
+		wsConnectionOpen: function() {
+			AFDLOG.ws = new WebSocket(
+				AFDLOG.urlWsProto + "//" + AFDLOG.urlBase + "/log",
+				["json"]
+			);
+			AFDLOG.ws.addEventListener("open", function() {
+				console.info("ws-connection open");
+			});
+			AFDLOG.ws.addEventListener("close", function() {
+				alert("AFD closed connection!");
+			});
+			AFDLOG.ws.addEventListener("error", function(event) {
+				// error handler -> reconnect.
+				console.warn(event);
+			});
+			AFDLOG.ws.addEventListener("message", function(event) {
+				const message = JSON.parse(event.data);
+				console.debug(message);
+				/* evaluate incoming message */
+				if (message.class == "log") {
+					let context = $("#" + message.context + "-area");
+					if (message.append) {
+						// TODO append or replace lines.
+					}
+					if ("lines" in message) {
+						context.html(message.lines);
+					}
+					else {
+						context.html(message.text);
+					}
+					context.find("tr").on("click", function(event) {
+						$(this).toggleClass("selected");
+					});
+					$("." + message.context + "-area-scroll").scrollTop($(this)[0].scrollHeight);
+					/* TODO progress-swirl? */
+					$("#" + message.context + " *").css({
+						"cursor": "auto"
+					});
+
+				}
+			});
+		},
+
+		/**
+         * 
+         */
+		wsConnctionClose: function() {
+			console.debug("close ws connection.");
+		},
+
+        /**
+         * Retrieve full log-file content (e.g. system-log, transfer-log).
+         */
+		callAldaLevel: function(logName) {
+			console.debug("callAldaLevel " + logName);
+			var transl = {
+				info: "I",
+				config: "C",
+				warn: "W",
+				error: "E",
+				offline: "O",
+				debug: "D"
+			};
+			let levelList = [];
+			$.each($("#" + logName + " ." + logName + "-level"), function(i, v) {
+				if (v.checked) {
+					levelList.push(transl[v.value]);
+				}
+			});
+			if (levelList.length == 0) {
+				alert("Select at least one log-level!");
+				return false;
+			}
+			let fileNumber = $("#" + logName + "-logfile").get(0).value;
+			const msg = {
+				class: "log",
+				context: logName,
+				filter: {
+					file: fileNumber,
+					level: levelList.join("|")
+				}
+			}
+			console.debug(msg);
+			AFDLOG.ws.send(JSON.stringify(msg));
+		},
+
+        /**
+         * Retrieve log information with ALDA.
+         */
+		callAldaFilter: function(logName) {
+			console.debug("callAldaFilter " + logName);
+			let paramSet = {};
+			$("#" + logName + " *").css({
+				"cursor": "progress"
+			});
+			$.each($("#" + logName + " .filter"), function(i, obj) {
+				if (obj.type == "checkbox") {
+					if (obj.checked == true) {
+						paramSet[obj.name] = obj.value;
+					}
+				} else if (obj.value != "") {
+					paramSet[obj.name] = obj.value;
+				}
+			});
+			const msg = {
+				class: "log",
+				context: logName,
+				filter: paramSet
+			};
+			console.debug(msg);
+			AFDLOG.ws.send(JSON.stringify(msg));
+		},
+
+		/**
+		 * Set input fields for start- and end-time according to range.
+		 */
+		setDate: function(logName, timeRange) {
+			console.debug("setDate", logName, timeRange);
+			if (timeRange == "clear") {
+				$("#" + logName + " .filter[name=start]").val("");
+				$("#" + logName + " .filter[name=end]").val("");
+				return;
+			}
+			let dateStart = "";
+			let dateEnd = "";
+			let now = new Date(Date.now());
+			let datetimeNowMonth = now.getMonth() + 1;
+			let datetimeNowDay = now.getDate();
+			let datetimeNowHour = now.getHours();
+			let datetimeNowMinute = now.getMinutes();
+			let yday = new Date(Date.now());
+			yday.setDate(now.getDate() - 1);
+			let datetimeYdayMonth = yday.getMonth() + 1;
+			let datetimeYdayDay = yday.getDate();
+			if (datetimeNowMonth < 10) {
+				datetimeNowMonth = '0' + datetimeNowMonth;
+			}
+			if (datetimeNowDay < 10) {
+				datetimeNowDay = '0' + datetimeNowDay;
+			}
+			if (datetimeNowHour < 10) {
+				datetimeNowHour = '0' + datetimeNowHour;
+			}
+			if (datetimeNowMinute < 10) {
+				datetimeNowMinute = '0' + datetimeNowMinute;
+			}
+			if (datetimeYdayMonth < 10) {
+				datetimeYdayMonth = '0' + datetimeYdayMonth;
+			}
+			if (datetimeYdayDay < 10) {
+				datetimeYdayDay = '0' + datetimeYdayDay;
+			}
+			if (timeRange == "today") {
+				dateStart = "" + datetimeNowMonth + datetimeNowDay + "0000";
+				dateEnd = "" + datetimeNowMonth + datetimeNowDay + datetimeNowHour + datetimeNowMinute;
+			} else if (timeRange == "yesterday") {
+				dateStart = "" + datetimeYdayMonth + datetimeYdayDay + "0000";
+				dateEnd = "" + datetimeNowMonth + datetimeNowDay + "0000";
+			}
+			$("#" + logName + " .filter[name=start]").val(dateStart);
+			$("#" + logName + " .filter[name=end]").val(dateEnd);
+			console.debug(dateStart, dateEnd);
+		},
+
+		/**
+		 * Show or hide modal with selection form.
+		 */
+		toggleModal: function(modal) {
+			$.each($("#" + modal + " input.form-check-input"), function(i, obj) {
+				if (obj.checked == true) {
+					obj.checked = false;
+				} else {
+					obj.checked = true;
+				}
+			});
+		},
+
+		/**
+		 * Update selection form in modal on any change, for event-handler.
+		 */
+		updateModal: function(modalId) {
+			let checkedList = [];
+			$.each($("#" + modalId + " .form-check-input"), function(i, obj) {
+				if (obj.checked) {
+					checkedList.push(obj.value);
+				}
+			});
+			$("#" + modalId + "Value").attr("value", checkedList.join(","));
+		},
+
+		/**
+		 * View file content for each selected one in a new window.
+		 */
+		callView: function(logName) {
+			console.debug("callView " + logName);
+			let selectedLogAreaLines = [];
+			$.each($("#" + logName + " .selected"), function(i, obj) {
+				if (obj.childNodes[obj.childElementCount - 1].innerText == "Y") {
+					selectedLogAreaLines.push(obj.attributes["archive"].value);
+				}
+			});
+			if (selectedLogAreaLines.length == 0) {
+				alert("Select archived log entry first!");
+				return;
+			}
+			let mode = $("#" + logName + "-view-mode").text().split(" ")[1].toLowerCase();
+			console.debug("view", mode, selectedLogAreaLines);
+			$.each(selectedLogAreaLines, function(i, v) {
+				window.open(AFDLOG.urlViewProto + "//" + AFDLOG.urlBase + "/view/" + mode + "/" + v);
+			});
+		}
+	}; /* End returned object. */
+}();
+
